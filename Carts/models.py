cfb08429cc496620dde6c49aa2f413de5914b5d0
Istiak Hossen Shihab <Shihab16-623@diu.edu.bd>
2025-09-24 11:03:57 +0600

    Product rest of th code 2

--- Carts/models.py
+++ Carts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from Accounts.models import CustomUser
+from Products.models import Product
 # Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cart_id
+    
+class CartItem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,null=True)
+    qty = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+
+    def sub_total(self):
+        return self.product.discount_price * self.qty
+    
+    def __str__(self):
+        return f'CartItem: {self.product.name}'
+    
+    def __unicode__(self):
+        return self.product
+
--- Carts/views.py
+++ Carts/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from Products import models
+from . import models
+from django.core.exceptions import  ObjectDoesNotExist
 
 # Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_slug):
+    url = request.META.get('HTTP_REFERER')
+    product = models.Product.objects.get(slug = product_slug)
+    
+    try:
+        cart = models.Cart.objects.get(cart_id = _cart_id(request))
+    except models.Cart.DoesNotExist:
+        cart = models.Cart.objects.create(cart_id = _cart_id(request))
+    cart.save()
+
+    try:
+        cart_item = models.CartItem.objects.get(product=product,cart=cart)
+        cart_item.qty += 1
+        cart_item.save()
+
+    except models.CartItem.DoesNotExist:
+        cart_item = models.CartItem.objects.create(product=product, cart=cart, user=request.user, qty=1)
+        cart_item.save()
+
+    return redirect(url)
+
+def cart(request, total=0, qty=0, cart_item=None):
+    try:
+        grand_total = 0
+        total = 0
+        if request.user.is_authenticated:
+            cart_items = models.CartItem.objects.filter(user = request.user, is_active = True)
+        else:
+            cart = models.Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = models.CartItem.objects.filter(cart = cart, is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.discount_price * cart_item.qty)
+            qty += cart_item.qty
+        grand_total = float(total) + float(6.90)
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'qty': qty,
+        'cart_items': cart_items,
+        'grand_total': grand_total,
+    }
+    return render(request, 'carts/cart.html', context)
+
+def remove_cart(request,product_slug):
+    url = request.META.get('HTTP_REFERER')
+    product = get_object_or_404(models.Product, slug = product_slug)
+    try:
+        if request.user.is_authenticated:
+            cart_item = models.CartItem.objects.get(user = request.user, product=product)
+        else:
+           cart = models.Cart.objects.get(cart_id=_cart_id(request))
+           cart_item = models.CartItem.objects.get(cart = cart, product=product)
+        if cart_item.qty > 1:
+            cart_item.qty -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+
+    return redirect(url)
+
+def remove_cart_item(request,product_slug):
+    url = request.META.get('HTTP_REFERER')
+    product = get_object_or_404(models.Product, slug = product_slug)
+
+    if request.user.is_authenticated:
+            cart_item = models.CartItem.objects.get(user = request.user, product=product)
+    else:
+        cart = models.Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = models.CartItem.objects.get(cart = cart, product=product)
+    cart_item.delete()
+    return redirect(url)
